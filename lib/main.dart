86da17dfcae3d1e8a42842a96937dbce562393a0
Antonio <96915878+Ferno22@users.noreply.github.com>
2023-10-01 18:26:22 +0200

    Added the completed task and edit task screen with working functionality

--- lib/main.dart
+++ lib/main.dart
@@ -29,14 +29,45 @@ class MyAppState extends ChangeNotifier {
   var completedTasks = [];
 
   void addTask(String task) {
-    pendingTasks.add(task);
-    notifyListeners();
+    if (task.isEmpty) {
+      return;
+    } else if (completedTasks.contains(task)) {
+      completedTasks.remove(task);
+      pendingTasks.add(task);
+      notifyListeners();
+    } else {
+      pendingTasks.add(task);
+      notifyListeners();
+    }
   }
 
   void completeTask(String task) {
     pendingTasks.remove(task);
     completedTasks.add(task);
     notifyListeners();
+  }
+
+  void updateTask(String oldTask, String newTask) {
+    if (newTask.isEmpty) {
+      return;
+    } else if (completedTasks.contains(oldTask)) {
+      completedTasks.remove(oldTask);
+      completedTasks.add(newTask);
+      notifyListeners();
+    }
+    pendingTasks.remove(oldTask);
+    pendingTasks.add(newTask);
+    notifyListeners();
+  }
+
+  void deleteTask(String task) {
+    if (pendingTasks.contains(task)) {
+      pendingTasks.remove(task);
+      notifyListeners();
+    } else if (completedTasks.contains(task)) {
+      completedTasks.remove(task);
+      notifyListeners();
+    }
   }
 }
 
@@ -50,6 +81,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = PendingTasksPage();
+        break;
+      case 1:
+        page = CompletedTasksPage();
+        break;
+      default:
+        throw Exception('Invalid index: $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth > 600,
+                destinations: const [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.timelapse),
+                    label: Text('Pending'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.check_circle),
+                    label: Text('Completed'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class PendingTasksPage extends StatelessWidget {
   final TextEditingController _taskController = TextEditingController();
 
   @override
@@ -58,13 +144,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 80,
-        title: Padding(
-          padding: const EdgeInsets.only(top: 40.0),
+        toolbarHeight: 140,
+        title: const Padding(
+          padding: EdgeInsets.only(top: 10.0),
           child: Text(
-            widget.title,
-            style: const TextStyle(
-              fontSize: 30,
+            'Pending Tasks',
+            style: TextStyle(
+              fontSize: 40,
               fontWeight: FontWeight.bold,
             ),
           ),
@@ -77,7 +163,23 @@ class _MyHomePageState extends State<MyHomePage> {
               itemCount: appState.pendingTasks.length,
               itemBuilder: (context, index) {
                 return ListTile(
+                  leading: IconButton(
+                    icon: const Icon(Icons.check),
+                    onPressed: () {
+                      appState.completeTask(appState.pendingTasks[index]);
+                    },
+                  ),
                   title: Text(appState.pendingTasks[index]),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TaskDetailsPage(
+                          task: appState.pendingTasks[index],
+                        ),
+                      ),
+                    );
+                  },
                 );
               },
             ),
@@ -122,6 +224,129 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CompletedTasksPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 140,
+        title: const Padding(
+          padding: EdgeInsets.only(top: 10.0),
+          child: Text(
+            'Completed Tasks',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: appState.completedTasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: IconButton(
+              icon: const Icon(Icons.undo),
+              onPressed: () {
+                appState.addTask(appState.completedTasks[index]);
+              },
+            ),
+            title: Text(appState.completedTasks[index]),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class TaskDetailsPage extends StatefulWidget {
+  final String task;
+
+  const TaskDetailsPage({Key? key, required this.task}) : super(key: key);
+
+  @override
+  _TaskDetailsPageState createState() => _TaskDetailsPageState();
+}
+
+class _TaskDetailsPageState extends State<TaskDetailsPage> {
+  late TextEditingController _taskController;
+
+  @override
+  void initState() {
+    super.initState();
+    _taskController = TextEditingController(text: widget.task);
+  }
+
+  @override
+  void dispose() {
+    _taskController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        title: Text(widget.task),
+      ),
+      body: Center(
+        // text field to edit the details of a task
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _taskController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Task',
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    appState.deleteTask(widget.task);
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Delete'),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    appState.completeTask(widget.task);
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Complete'),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    appState.updateTask(
+                      widget.task,
+                      _taskController.text,
+                    );
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Update'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
